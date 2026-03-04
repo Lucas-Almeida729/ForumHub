498c196e3b429019ed1f7ab355fac34ea9d55131
Lucas-Almeida729 <lucas.almeida729@gmail.com>
2026-03-04 15:23:06 -0300

    Primeiro commit do projeto

---
+++ src/test/java/Challenge/ForumHub/ForumHubApplicationTests.java
@@ -0,0 +1,13 @@
+package Challenge.ForumHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
